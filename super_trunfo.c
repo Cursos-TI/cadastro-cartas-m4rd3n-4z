68bd5104b0e5ea250102711e8e1272018cf54f9a
Marden do Carmo Azevedo <mardenmardenazevedo.ti@gmail.com>
2025-10-20 20:50:21 -0300

    Primeiro commit de inicializacao do projeto

---
+++ super_trunfo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+// Variáveis carta 1
+char estado1;                //Estado em (char)
+char codigo1 [4];            // Código da carta (string com espaço para 3 caractéres + '\0' )
+char nomeCidade1 [20];       // Nome da Cidade com até 19 caracteres
+int popuracao1;              // População em números inteiros (int)
+float area1;                 //Área em km² (float)
+float pib1;                  // Pib em milhões (float)         
+int pontosTuristicos1;       // Número de pontos turísticos (int)
+
+// Variáveis carta 2
+char estado2;                //Estado em (char)
+char codigo2 [4];            // Código da carta (string com espaço para 3 caractéres + '\0' )
+char nomeCidade2 [20];       // Nome da Cidade com até 19 caracteres
+int popuracao2;              // População em números inteiros (int)
+float area2;                 //Área em km² (float)
+float pib2;                  // Pib em milhões (float)         
+int pontosTuristicos;       // Número de pontos turísticos (int)
+
+// Entrada de dados da carta 1
+
+}
